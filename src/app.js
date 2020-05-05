14b67bdaef879a88cac03af616d3bcbe35069814
Sara Cruz <sara.cruz@secret-source.eu>
2020-05-05 13:22:01 +0100

    Adding address request query

--- src/app.js
+++ src/app.js
@@ -38,6 +38,32 @@ app.get('/help', (req, res) => {
     });
 })
 
+app.get('/weather', (req, res) => {
+  if(!req.query.address){
+     return res.send({
+         error: 'you must provide an address'
+     })
+  }
+  console.log(req.query.address)  
+  res.send({
+      forescast: 'It is snowing',
+      location: 'Philadelphia', 
+      address: req.query.address
+    })
+})
+
+app.get('/products', (req, res) => {
+    if(!req.query.search){
+       return res.send({ 
+           error: 'you must provide a search term'
+       })
+    }
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
 app.get('/help/*', (req, res) => {
       res.render("help*", {
       title: "Help article not found",
