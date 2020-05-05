e8e39390cf72635d4718e9d49fa4f1e3c4c2b9db
Sara Cruz <sara.cruz@secret-source.eu>
2020-05-05 17:16:43 +0100

    search location forecast working

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,21 @@
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
 
-fetch("http://localhost:3000/weather?address=boston").then((response) => {
+
+
+const weatherForm = document.querySelector('form');
+const searchElement = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const location = searchElement.value
+  fetch("http://localhost:3000/weather?address=" + location).then((response) => {
     response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location);
-            console.log(data.forecast);
-        }
-    })
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data.location);
+        console.log(data.forecast);
+      }
+    });
+  });
 })
